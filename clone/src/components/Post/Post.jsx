e27b0627f2a6fd0f41439c18a6467b69bec0105d
sungyuha <yuha822@naver.com>
2022-10-14 18:08:36 +0900

    react avatar

--- clone/src/components/Post/Post.jsx
+++ clone/src/components/Post/Post.jsx
@@ -1,10 +1,25 @@
 import React, { useState } from 'react'
+import { Avatar } from '@mui/material';
+// import { VerifiedUserIcon } from '@mui/icons-material';
 import './post.scss';
 
 const Post = () => {
     return (
         <div className="Post">
-            Hello from the post
+            <div className="post-avatar">
+                <Avatar />
+                <div className="post-body">
+                    <div className="post-header">
+                        <div className="post-headerText">
+                            <h3>DisplayName</h3>
+                            <span className="post-headerSpecial">
+                                {/* <VerifiedUserIcon /> */}
+                                @username
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
